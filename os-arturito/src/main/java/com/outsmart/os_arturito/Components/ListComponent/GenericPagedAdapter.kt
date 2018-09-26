c21eb504bf4c7099f33ad42313b0653e484284b5
Rudi Eros De Francesco <rudierosdfr@gmail.com>
2018-09-26 00:20:40 -0300

    init

---
+++ os-arturito/src/main/java/com/outsmart/os_arturito/Components/ListComponent/GenericPagedAdapter.kt
@@ -0,0 +1,52 @@
+package com.outsmart.os_arturito.Components.ListComponent
+
+import android.arch.paging.PagedListAdapter
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by rudieros on 19/08/18.
+ */
+
+
+class GenericPagedAdapter<T>(
+        private val layout: Int,
+        private var data: List<ListItem>?
+) : PagedListAdapter<T, GenericViewHolder>() {
+
+    var listener: GenericAdapterListener? = null
+
+    public fun setData(data: List<ListItem>) {
+        this.data = data
+        notifyDataSetChanged()
+    }
+
+    public fun setAdapterListener(listener: GenericAdapterListener?) {
+        this.listener = listener
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GenericViewHolder {
+        // create a new view
+        val view = LayoutInflater.from(parent.context)
+                .inflate(layout, parent, false)
+        // set the view's size, margins, paddings and layout parameters
+        return GenericViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(holder: GenericViewHolder, position: Int) {
+        data?.get(position)?.bindView(holder.view)
+        Log.d("Generic Adapter:", "Bind" )
+        if (data?.size?.minus(1) == position) {
+            this.listener?.paginate()
+            Log.d("Generic Adapter:", "Last element")
+        }
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = data?.size ?: 0
+}
